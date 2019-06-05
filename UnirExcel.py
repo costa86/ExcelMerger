a55fb75e6cfc24e9daa509dcf3c680f50ae5558e
Lourenço Costa <costa86@zoho.com>
2019-06-05 18:54:08 -0300

    Update UnirExcel.py

--- UnirExcel.py
+++ UnirExcel.py
@@ -1,6 +1,7 @@
 import os,time
 import pandas as pd
-instruc='''Instructions:
+
+instruc = '''Instructions:
 1.All the Excel files (.xls or .xlsx) in this folder will be merged into a new single file organized by sheets according to the chosen columm
 2.Any other files with different extensions will be ignored, so don't worry about them.
 ------------------------------------------------------------------------- 
@@ -15,15 +16,23 @@ for cada in os.listdir():
 frames = []
 for i in arquivos:
     a = pd.read_excel(i)
-    frames.append(a)
+    frames.append(a) 
 dataFrame = pd.concat(frames)
+colunas = [i for i in dataFrame.columns]
 print('Available columms: ')
-for i in dataFrame.columns: 
-    print (i)
-coluna = input('Input file - pick a columm to generate sheets from: ')
-abas = dataFrame[coluna].unique()
+while True:
+    try:
+        for i,n in enumerate(colunas): 
+            print (i,n)
+        coluna = int(input('Input file - pick a columm to generate sheets from: '))
+    except Exception:
+        continue
+    if coluna in range(0,len(colunas)):
+        break
+esc = colunas[coluna]
+abas = dataFrame[esc].unique()
 with pd.ExcelWriter(destino+'.xls') as writer:
         for i in abas:
-            dataFrame[dataFrame[coluna]==i].to_excel(writer,index=False,sheet_name=(str(i)))
+            dataFrame[dataFrame[esc]==i].to_excel(writer,index=False,sheet_name=(str(i)))
 print(f'File {destino} created successfully!')			
 time.sleep(3)
